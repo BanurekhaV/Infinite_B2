c0ea98ab3951110c16f85ee3a4e6de30d5557990
BanurekhaV <rekhasairam@hotmail.com>
2023-09-04 18:26:33 +0530

    My First Commit

---
+++ Day1_SimpleConsole/Day1SimpleConsole_2/SecondProgram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1SimpleConsole_2
+{
+    class SecondProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Project no. 2.");
+            Console.WriteLine("We are trying to understand assemblies...");
+            Console.ReadLine(); // this line of code ensures the program awaits users 
+                            // input before exiting                            
+        }
+    }
+}
