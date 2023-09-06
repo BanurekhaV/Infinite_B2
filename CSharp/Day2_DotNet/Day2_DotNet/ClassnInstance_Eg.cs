4da0d3d1a67548b1407db3758a109f0eb52b30c9
BanurekhaV <rekhasairam@hotmail.com>
2023-09-06 13:33:41 +0530

    Day3 Dotnet pre-lunch

--- CSharp/Day2_DotNet/Day2_DotNet/ClassnInstance_Eg.cs
+++ CSharp/Day2_DotNet/Day2_DotNet/ClassnInstance_Eg.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using static System.Console;
 
 namespace Day2_DotNet
 {
@@ -12,10 +12,9 @@ namespace Day2_DotNet
             ClassnInstance_Eg cobj2 = new ClassnInstance_Eg();
             Console.WriteLine(cobj.Add(5, 7)); // invoking an instance method
             Console.WriteLine(cobj2.Add(10, 20));
-           // evenNos(4);  //invoking a class/static method
-                          ClassnInstance_Eg.evenNos(7);
-            Console.Read();
-            
+            staticEg.evenNos(4);  //invoking a class/static method that is in another class
+            oddNos(6); //invoking a class/static method that is within the same class
+             Read();            
         }
 
         public int Add(int x, int y) // instance functions/methods
@@ -23,9 +22,20 @@ namespace Day2_DotNet
             return x + y;
         }
 
+        public static void oddNos(int x)
+        {
+            if(x%2!=0)
+                Console.WriteLine("Number is Odd");
+            else
+                Console.WriteLine("Number is Even");
+        }
+    }
+
+    class staticEg
+    {
         public static void evenNos(int num) // static functions are known as class functions/methods
         {
-            if(num%2==0)
+            if (num % 2 == 0)
                 Console.WriteLine("Number is an even Number");
             else
                 Console.WriteLine("Number is an Odd Number");
