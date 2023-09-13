7aa262a504bfcde676b73b2a142d708a57dc9a36
BanurekhaV <rekhasairam@hotmail.com>
2023-09-13 11:47:39 +0530

    interfaces

---
+++ CSharp/DotNet_Day8/DotNet_Day8/Interface_Eg.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Day8
+{
+   public interface ICustomer  // prefix I before the interface name
+    {
+        void PrintCustomer();
+        int AddNos(int x, int y);        
+    }
+
+    interface ISupplier
+    {
+        void PrintSupplier();
+    }
+    class Customer : ICustomer, ISupplier
+    {
+        public int AddNos(int x, int y)
+        {
+            return x + y;
+        }
+
+        public void PrintCustomer()
+        {
+            Console.WriteLine("Interface ICustomer Print");
+        }
+
+        public void PrintSupplier()
+        {
+            Console.WriteLine("Interface ISupplier Print");
+        }
+    }
+    class Interface_Eg
+    {
+        static void Main()
+        {
+            Customer cust = new Customer();
+            Console.WriteLine("Customer and Supplier Printing using Customer Object");
+            cust.PrintCustomer(); 
+            cust.PrintSupplier();
+
+            ICustomer ic;
+            ic = new Customer();
+
+            Console.WriteLine("Customer Printing using ICustomer Object");
+            ic.PrintCustomer();
+            
+            ISupplier isupp;
+            isupp = new Customer();
+            Console.WriteLine("Supplier Printing using ISupplier Object");
+            isupp.PrintSupplier();
+            Console.Read();
+        }
+       
+    }
+}
---
+++ CSharp/DotNet_Day8/DotNet_Day8/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DotNet_Day8
+{
+    class Engine
+    {
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public double Power { get; set; }
+
+        public Engine(string model,string make, double power)
+        {
+            Model = model;
+            Make = make;
+            Power = power;
+        }
+    }
+
+    class Car
+    {
+        public string carType { get; set; }
+        public string carName { get; set; }
+        public double carCost { get; set; }
+        public Engine carEngine { get; set; }
+
+        public Car(string carType,string carName, double carCost, Engine carEngine)
+        {
+            this.carType = carType;
+            this.carName = carName;
+            this.carCost = carCost;
+            this.carEngine = carEngine; // will call the Engine class constructor
+        }
+
+        public void DisplayCar()
+        {
+            Console.WriteLine($"Car Name {carName}, Car cost {carCost}, Engine Make {carEngine.Make}, Car Model {carEngine.Model}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Engine e = new Engine("E1", "Ford", 1235667);
+            Car c = new Car("SUV", "EcoSport", 1700000, e);
+            c.DisplayCar();
+            Console.Read();
+        }
+    }
+}
