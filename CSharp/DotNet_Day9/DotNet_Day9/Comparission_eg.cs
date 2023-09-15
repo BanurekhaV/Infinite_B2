2411823f6a5107f94f51046c75a60602f5ae3bb0
BanurekhaV <rekhasairam@hotmail.com>
2023-09-15 13:33:51 +0530

    Equalitychecks

---
+++ CSharp/DotNet_Day9/DotNet_Day9/Comparission_eg.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Day9
+{
+    class Comparission_eg
+    {
+        static void Main()
+        {
+            string[] fruits = { "Orange", "Banana", "Apple", "Guava" };
+            Array.Sort(fruits);
+            foreach (string s in fruits)
+            {
+                Console.WriteLine(s);
+            }
+
+            Students stud = new Students();
+            stud.compareStudentsName();
+            Console.WriteLine("Comparing Marks ---------");
+            Marks m = new Marks();
+            Students s1 = new Students() { Name = "Natasha", Totalmarks = 81 };
+            Students s2 = new Students() { Name = "Aasha", Totalmarks = 86 };
+            Console.WriteLine(m.Compare(s1, s2)); 
+            Console.Read();
+        }
+    }
+
+    class Students : IComparable
+    {
+        public string Name { get; set; }
+        public int Totalmarks { get; set; }
+
+        public void compareStudentsName()
+        {
+            //Object initialization
+            Students[] studarray = new Students[]
+            {
+                new Students(){Name="Deepika", Totalmarks=68},
+                new Students(){Name="Amarnath",Totalmarks=72},
+                new Students(){ Name="HimaBindu",Totalmarks=75},
+                 new Students(){ Name="Adithya",Totalmarks=70}
+            };
+            Array.Sort(studarray);
+            foreach(var s in studarray)
+            {
+                Console.WriteLine(s.Name);
+            }
+        }
+
+        public int CompareTo(object obj)
+        {
+            Students sobj = obj as Students; // (typecasting object to students)
+            if(sobj !=null)
+            {
+                return this.Name.CompareTo(sobj.Name);
+            }
+            return -1;
+        }
+    }
+
+    class Marks : IComparer<Students>
+    {
+                   
+    public int Compare(Students x, Students y)
+        {
+             return x.Totalmarks.CompareTo(y.Totalmarks);
+        }
+                          
+    }
+}
+
---
+++ CSharp/DotNet_Day9/DotNet_Day9/EqualsOperations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Day9
+{
+    class EqualsOperations
+    {
+        static void Main()
+        {
+            //reference equals with string literals
+            string s1 = "a";
+            // string s2 = "a"; returns true
+            string s2=s1; // true
+            //string s2 = new string('h', 'i');  false
+           // string s2 = "b";  //returns false
+            //Console.WriteLine(object.ReferenceEquals(s1,s2));
+            StringBuilder sb1 = new StringBuilder();
+            // StringBuilder sb2 = new StringBuilder();
+            StringBuilder sb2 = sb1;
+            Console.WriteLine(object.ReferenceEquals(sb1,sb2));
+            Console.WriteLine("----------------");
+            Console.WriteLine(s1.Equals(s2));
+
+            //compareTo with value types
+            Console.WriteLine("CompareTo with Value Types");
+            const int x= 5;
+            const int y = 6;
+            const int z = 5;
+
+            Console.WriteLine(x.CompareTo(y)); //returns -1 (first numeber is small)
+            Console.WriteLine(x.CompareTo(z)); //returns 0 (both are same)
+            Console.WriteLine(y.CompareTo(z)); //returns 1 (first is big)
+                       
+            Console.Read();
+        }
+    }
+}
