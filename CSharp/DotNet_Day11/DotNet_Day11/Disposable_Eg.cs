59ff07962ffa4cb8235528e5b3cbb067d8b059ed
BanurekhaV <rekhasairam@hotmail.com>
2023-09-19 16:28:13 +0530

    GC + Dispose

---
+++ CSharp/DotNet_Day11/DotNet_Day11/Disposable_Eg.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Day11
+{
+    class Disposable_Eg : IDisposable
+    {
+        //constructor
+        public Disposable_Eg()
+        {
+            Console.WriteLine("Allocating Resources...");
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Using Resources for application..");
+        }
+        public void Dispose()
+        {
+            Console.WriteLine("Releasing Resources..");
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Disposable Example 1...");
+            using(var disobj= new Disposable_Eg())
+            {
+                disobj.DoSomething();
+            }  // the using block will call the dispose() automatically
+
+            Console.WriteLine("Disposable Example 2...");
+
+            using (StreamReader sr = new StreamReader(@"C:\Banu\Infinite\B2\Git basics.txt"))
+            {
+                Console.WriteLine(sr.ReadToEnd());
+            }
+                Console.Read();
+        }
+    }
+}
---
+++ CSharp/DotNet_Day11/DotNet_Day11/EventsEg2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Day11
+{
+    class EventsEg2
+    {
+        int data = 5;
+        public delegate void NumberManipulator(int x);
+
+        public event NumberManipulator ChangeNumEvent;
+
+        public EventsEg2(int n)
+        {
+            this.ChangeNumEvent += new NumberManipulator(this.OnChangeNum); //registering an event with the delegate and hence a function
+            setValue(n);
+        }
+
+        public void OnChangeNum(int x) // event handler
+        {
+           // data = x;
+            Console.WriteLine($"Event Raised and the value from {data} is being changed to {x}");
+        }
+
+        public void setValue(int n) // responsible for raising an event 
+        {
+            
+            if (data != n)   //data's value and input value are different
+            {
+                ChangeNumEvent(n);
+            }
+            else
+            {
+                Console.WriteLine("No Changes in the Value.. No Event");
+            }
+        }
+    }
+
+    class TestEvent
+    {
+        static void Main()
+        {
+            EventsEg2 eobj2 = new EventsEg2(5);  // No Event
+            eobj2.setValue(15);  // Event will be raised
+            eobj2.setValue(20);
+            eobj2.setValue(5);
+            eobj2 = null;
+            GC.Collect(); // forcefull call to collect the objects released
+            Console.WriteLine("Garbage Programming");
+            Console.WriteLine("the Number of Generations are :" + GC.MaxGeneration);
+           // Console.WriteLine("The generation number of the Object eobj is " + GC.GetGeneration(eobj2));
+            Console.WriteLine("Total memory :" + GC.GetTotalMemory(false));
+
+            Console.WriteLine("------------");
+            EventsEg2 eobjnew = new EventsEg2(6);
+            Console.WriteLine("Garabage Collection in Generation 0 is:" + GC.CollectionCount(0));
+            Console.WriteLine("The generation number of the Object eobj is " + GC.GetGeneration(eobjnew));
+            Console.WriteLine("Total memory :" + GC.GetTotalMemory(false));
+            Console.Read();
+        }
+    }
+}
