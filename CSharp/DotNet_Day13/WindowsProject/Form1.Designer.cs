631dd9f578ee4cd73e975fbb876df4123eedaa03
BanurekhaV <rekhasairam@hotmail.com>
2023-09-21 13:40:07 +0530

    Windows Application

---
+++ CSharp/DotNet_Day13/WindowsProject/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace WindowsProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_msg = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_msg
+            // 
+            this.btn_msg.BackColor = System.Drawing.SystemColors.Window;
+            this.btn_msg.Location = new System.Drawing.Point(443, 81);
+            this.btn_msg.Name = "btn_msg";
+            this.btn_msg.Size = new System.Drawing.Size(87, 23);
+            this.btn_msg.TabIndex = 0;
+            this.btn_msg.Text = "Click Me";
+            this.btn_msg.UseVisualStyleBackColor = false;
+            this.btn_msg.Click += new System.EventHandler(this.btn_msg_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(440, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(488, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "New Label";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(176, 182);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(188, 20);
+            this.txtName.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 450);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_msg);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_msg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
+
---
+++ CSharp/DotNet_Day13/WindowsProject/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }        
+       
+        //method that will count the number of characters in a given file
+         private int CountCharacters()
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader("DataFile.txt"))
+            {
+                string content = reader.ReadToEnd();
+                count = content.Length;  //counts the no.of characters
+                Thread.Sleep(5000);
+            }
+            return count;
+        }
+
+        //this event is running synchronously
+        //private void btn_msg_Click(object sender, EventArgs e)
+        //{
+        //    label2.Text = txtName.Text;
+        //    label2.Visible = true;
+        //    label2.Enabled = false;
+        //    int ch = CountCharacters(); //calling the func
+        //    label2.Text = "Processing character count, Please wait ....";
+        //    label2.Text = ch.ToString();
+        //}
+
+        //asynchronously
+        private async void btn_msg_Click(object sender, EventArgs e)
+        {
+            Task<int> mytask = new Task<int>(CountCharacters);
+            mytask.Start();
+            label2.Text = "Processing Character Count, please wait...";
+            txtName.Text = "Waiting for count value";
+            int ch = await mytask;
+            label2.Text = ch.ToString() + " " + "Characters were found in the file";
+
+        }
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome to Windows..");
+        }
+    }
+}
