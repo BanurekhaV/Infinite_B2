0b616d8a75203f0694c0d1fe792a87c3c04c0157
BanurekhaV <rekhasairam@hotmail.com>
2023-09-21 11:26:37 +0530

    Threads and Tasks

---
+++ CSharp/DotNet_Day12/DotNet_Day12/ThreadPool.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNet_Day12
+{
+    class ThreadPool
+    {
+        static void Main()
+        {
+            // No Thread Pools
+            //for(int i=0; i<10;i++)
+            //{
+            //    Thread thread = new Thread(Method1)
+            //    {
+            //        Name = "Thread" + i
+            //    };
+            //    thread.Start();
+            //}
+
+            //with Thread Pool
+            for(int i=0; i<10; i++)
+            {
+               System.Threading.ThreadPool.QueueUserWorkItem(new WaitCallback(Method1));
+            }
+           
+            Console.Read();
+        }
+
+        public static void Method1(object o)
+        {
+            Thread thread = Thread.CurrentThread;
+            string msg = $"Background : {thread.IsBackground}, Thread Pool : {thread.IsThreadPoolThread}," +
+                $"Thread ID : {thread.ManagedThreadId}";
+            Console.WriteLine(msg);
+        }
+    }
+}
---
+++ CSharp/DotNet_Day13/DotNet_Day13/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Day13
+{
+    class Accounts
+    {
+       public double Balance;
+
+        public Accounts(double d)
+        {
+            Balance = d;
+        }
+
+        public  void Credit(double amt)
+        {
+            Balance += amt;
+        }
+
+        public void Debit(double amt)
+        {
+            Balance -= amt;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new Accounts(1000);
+            var tasks = new Task[50];
+            for(int i=0; i<tasks.Length; i++)
+            {
+                tasks[i] = Task.Run(() => RandomlyUpdate(account));
+            }
+            Task.WaitAll(tasks);
+            Console.WriteLine("All Tasks Done..");
+            Console.WriteLine(account.Balance);
+            Console.Read();
+        }
+
+        static void RandomlyUpdate(Accounts account)
+        {
+            var rand = new Random();
+            for(int i=0; i<10; i++)
+            {
+                var amount = rand.Next(1, 100);   //25
+
+                bool b = rand.NextDouble() < 0.5;
+                if(b)
+                {
+                    account.Credit(amount);
+                }
+                else
+                {
+                    account.Debit(amount);
+                }
+
+            }
+        }
+    }
+}
