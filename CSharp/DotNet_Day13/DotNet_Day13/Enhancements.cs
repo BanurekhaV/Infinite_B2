8972b761bfe782e84797ff67f828287f2bce069a
BanurekhaV <rekhasairam@hotmail.com>
2023-09-21 18:50:03 +0530

    Enhancements and Assignment 6

--- CSharp/DotNet_Day13/DotNet_Day13/Enhancements.cs
+++ CSharp/DotNet_Day13/DotNet_Day13/Enhancements.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using CalculatorDLL;
 using Calculator2;
+using System.Runtime.InteropServices;
 
 namespace DotNet_Day13
 {
@@ -52,14 +53,59 @@ namespace DotNet_Day13
             CalculatorClass cc = new CalculatorClass();
             Console.WriteLine(cc.WelcomeFunc("Deepika"));
             Console.WriteLine("Addition : {0}",cc.Add2Nos(10,20));
+
+            //using the extension methods with the original class object
             Console.WriteLine("Multiplication :{0}",cc.Multiply2Nos(10,20));
             Console.WriteLine("Subtraction :{0}",cc.Subtract2Nos(20,10));
+
+            //Named and optional Parameters
+            Console.WriteLine("***********************");
+            NamedOptional no = new NamedOptional();
+            Console.WriteLine(no.AddNumber(15,20)); // positional
+
+            Console.WriteLine(no.AddNumber(firstnumber:20,secondnumber:15));  //named parameter
+            Console.WriteLine(no.AddNumber(secondnumber:10, firstnumber:5)); // named parameter, changing the order of the parameters
+
+            //1. Named arguments can follow positional arguments
+            Console.WriteLine(no.AddNumber(2,secondnumber:5));
+
+            //2. Positional arguments cannot follow named arguments. It throws compilation error
+            //  Console.WriteLine(no.AddNumber(secondnumber:8, 10));
+
+            Console.WriteLine("-----Default valued Parameters-----");
+            Console.WriteLine(no.Add(15)); // passing just one parameter, the other will take default value
+            Console.WriteLine(no.Add(15,100));  //default value will be overwritten by the passed value
+
+            Console.WriteLine("------Optional Parameters------");
+            Console.WriteLine(no.Sum(25));  // the second parameter is optional
+            Console.WriteLine(no.Sum(25,25));
             Console.Read();
         }
 
         public void Display()
         {
             Console.WriteLine("Hi");
+        }
+    }
+
+    class NamedOptional
+    {
+       public int AddNumber(int firstnumber, int secondnumber)
+        {
+            return firstnumber + secondnumber;
+        }
+
+        //default value for a parameter
+        public int Add(int a, int b=5)
+        {
+            return a + b;
+        }
+
+        //optional parameters
+      
+        public int Sum(int num1, [Optional] int num2)
+        {
+            return num1 + num2;
         }
     }
 }
