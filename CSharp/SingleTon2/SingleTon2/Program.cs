c0e683e5d43c1fa16c882d25257b1e527ade0fb7
BanurekhaV <rekhasairam@hotmail.com>
2023-09-26 12:40:18 +0530

    Singleton Pattern

---
+++ CSharp/SingleTon2/SingleTon2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SingleTonPattern;
+
+namespace SingleTon2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton training = Singleton.GetInstance();
+            training.Print("this is the Training..");
+            Console.Read();
+
+        }
+    }
+}
