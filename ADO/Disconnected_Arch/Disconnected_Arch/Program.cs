044c7d2234dc2a70fa00e6ea6478ebf2d689748a
BanurekhaV <rekhasairam@hotmail.com>
2023-10-16 13:37:41 +0530

    disconnected

---
+++ ADO/Disconnected_Arch/Disconnected_Arch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Disconnected_Arch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con;
+            SqlDataAdapter da;
+            try
+            {
+                con = new SqlConnection("Data Source=Laptop-tjj7d977; initial Catalog=Northwind;" +
+                    "trusted_connection=true;");
+                da = new SqlDataAdapter("select * from region", con);
+                con.Open();
+                DataSet ds = new DataSet();
+                da.Fill(ds, "nRegion");
+                DataTable dt = ds.Tables["nRegion"];
+
+                //to access the data in the datatable 
+                foreach(DataRow drow in dt.Rows)
+                {
+                    foreach(DataColumn dcol in dt.Columns)
+                    {
+                        Console.Write(drow[dcol]);
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch(SqlException se)
+            {
+                Console.WriteLine(se.Message);
+            }
+            Console.Read();
+        }
+    }
+}
