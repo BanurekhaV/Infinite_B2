82a0c66e9ddcdf0244343654bfaf0e5d1bb331b2
BanurekhaV <rekhasairam@hotmail.com>
2023-10-18 11:33:59 +0530

    Linqtosql

---
+++ EF/LInqToSQL/LInqToSQL/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LInqToSQL
+{
+    class Program
+    {
+        static NorthWindDataDataContext ndc = new NorthWindDataDataContext();
+        static void Main(string[] args)
+        {
+            var cat = ndc.Categories.ToList();
+
+            foreach(var c in cat)
+            {
+                Console.WriteLine($"{c.CategoryID} {c.CategoryName}, {c.Description}");
+            }
+            Console.WriteLine("--------------------");
+            var cust = ndc.Customers.ToList();
+
+            foreach(var customer in cust)
+            {
+                Console.WriteLine($"{customer.ContactName},{customer.City}, {customer.Country}");
+            }
+            Console.Read();
+            
+        }
+    }
+}
---
+++ EF/LinqToSql_SqlMetal/LinqToSql_SqlMetal/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LinqToSql_SqlMetal
+{
+    class Program
+    {
+        static NorthwindContext db = new NorthwindContext();
+        
+        static void Main(string[] args)
+        {
+            var emp = from e in db.Employees
+                      orderby e.FirstName
+                      select e;
+
+            //or
+
+           // var emp1 = db.Employees.ToList();
+            foreach(var v in emp)
+            {
+                Console.WriteLine($"{v.EmployeeID}, {v.FirstName}, {v.LastName}, {v.HireDate}");
+            }
+            Console.Read();
+        }
+    }
+}
