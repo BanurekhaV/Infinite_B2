5ad6eadaaed39428b7ad38a2d0213f9aaa3b688f
BanurekhaV <rekhasairam@hotmail.com>
2023-11-04 11:54:49 +0530

    Scaffolded Controller with Views

--- MVC/MVC_EFPrj/MVC_EFPrj/Controllers/CategoryController.cs
+++ MVC/MVC_EFPrj/MVC_EFPrj/Controllers/CategoryController.cs
@@ -50,6 +50,58 @@ namespace MVC_EFPrj.Controllers
             db.SaveChanges();
             return RedirectToAction("GetCategoryScaffold");
         }
+        public ActionResult Delete(int id)
+        {
+            Category cat = db.Categories.Find(id);
+            return View(cat);
+        }
+        //delete option
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Category cat = db.Categories.Find(id);
+            db.Categories.Remove(cat);
+            db.SaveChanges();
+            return RedirectToAction("GetCategoryScaffold");
+        }
+
+        //edit
+        public ActionResult Update(int id)
+        {
+            Category c = db.Categories.Find(id);
+            return View(c);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Category category)
+        {
+            Category c = db.Categories.Find(category.CategoryID);
+            c.CategoryName = category.CategoryName;
+            c.Description = category.Description;
+            c.Picture = category.Picture;
+            db.SaveChanges();
+            return RedirectToAction("GetCategoryScaffold");
+        }
+
+        //details
+        public ActionResult Details(int id)
+        {
+            Category c = db.Categories.Find(id);
+            return View(c);
+        }
+
+        //get category by category name sorted
+        public ActionResult GetCategoryByName()
+        {
+            List<string> c = (from cat in db.Categories
+                              orderby cat.CategoryName
+                              select cat.CategoryName).ToList();
+
+           // dynamic c1 = db.Categories.OrderBy(cat => cat.CategoryName).ToList();
+            return View(c);
+            //List<string> ls = c1 as List<string>;
+            //return View(ls);
+        }
 
     }
 }
